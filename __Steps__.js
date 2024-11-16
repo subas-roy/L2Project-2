8f885bcb3ccac0931a258022b45e49a63e59aa7f
subas-roy <subasroy46@gmail.com>
2024-11-16 23:27:06 +0600

    Install express, mongoose, typescript, cors and dotenv

---
+++ __Steps__.js
@@ -0,0 +1,29 @@
+/***
+ * ------------------------------------
+ *    initialize package.json file
+ * ------------------------------------
+ * 1. npm init -y 
+ * 
+ * ----------------------------------------------------
+ *    install express, mongoose, typescript and cors 
+ * ----------------------------------------------------
+ * 2. npm install express
+ * 3. npm install mongoose --save
+ * 4. npm install typescript --save-dev
+ * 5. npm install cors
+ * 
+ * ----------------------------------------
+ *    dotenv - for evironment variables
+ * ----------------------------------------
+ * 6. npm install dotenv
+ * 
+ * -------------------------------------
+ *    initialize typescript.json file
+ * -------------------------------------
+ * 7. tsc -init
+ *    * in 'tsconfig.json' set "rootDir": "/src" and "outDir": "/dist"
+ * 
+ * 8. npm i --save-dev @types/node
+ * 9. 
+ *  
+ */

ac885c7faa3a72d6a8231b2b56edf2d60bd36347
subas-roy <subasroy46@gmail.com>
2024-11-17 16:32:01 +0600

    Install prettier, fix formatting issues and project basic setup done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,24 @@ import tseslint from 'typescript-eslint';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     ignores: ['node_modules', 'dist'],
     rules: {
       'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
     },
   },
 ];

9a03cf0e92aa8205935c9405e74aeb0d95ca62a0
subas-roy <subasroy46@gmail.com>
2024-11-21 15:49:57 +0600

    Usage of trim setter

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -10,14 +10,17 @@ const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
     required: [true, 'first name is required'],
+    trim: true,
     maxlength: [20, 'Frist name can not be more than 20 characters'],
   },
   middleName: {
     type: String,
+    trim: true,
   },
   lastName: {
     type: String,
     required: [true, 'Last name is required'],
+    trim: true,
   },
 });
 
@@ -25,26 +28,32 @@ const guardianSchema = new Schema<Guardian>({
   fatherName: {
     type: String,
     required: [true, 'Father name is required'],
+    trim: true,
   },
   fatherOccupation: {
     type: String,
     required: [true, 'Father occupation is required'],
+    trim: true,
   },
   fatherContactNo: {
     type: String,
     required: [true, 'Father contact no. is required'],
+    trim: true,
   },
   motherName: {
     type: String,
     required: [true, 'Mother name is required'],
+    trim: true,
   },
   motherOccupation: {
     type: String,
     required: [true, 'Mother occupation is required'],
+    trim: true,
   },
   motherContactNo: {
     type: String,
     required: [true, 'Mother contact no. is required'],
+    trim: true,
   },
 });
 
@@ -52,18 +61,22 @@ const localGuardianSchema = new Schema<localGuardian>({
   name: {
     type: String,
     required: [true, 'Local guardian name is required'],
+    trim: true,
   },
   occupation: {
     type: String,
     required: [true, 'Local guardian occupation is required'],
+    trim: true,
   },
   contactNo: {
     type: String,
     required: true,
+    trim: true,
   },
   address: {
     type: String,
     required: [true, 'Local guardian address is required'],
+    trim: true,
   },
 });
 
@@ -86,10 +99,12 @@ const studentSchema = new Schema<Student>({
   contactNo: {
     type: String,
     required: [true, 'Student contact no. is required'],
+    trim: true,
   },
   emergencyContactNo: {
     type: String,
     required: [true, 'Emergency contact no. is required'],
+    trim: true,
   },
   bloodGroup: {
     type: String,

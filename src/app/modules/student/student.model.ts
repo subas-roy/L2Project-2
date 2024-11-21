634fe47391eb7c59ac3b76bef4bef5227e43c2e5
subas-roy <subasroy46@gmail.com>
2024-11-21 10:13:36 +0600

    Usage of mongoose built-in validators

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,118 @@
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  localGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: [true, 'first name is required'],
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: [true, 'Father name is required'],
+  },
+  fatherOccupation: {
+    type: String,
+    required: [true, 'Father occupation is required'],
+  },
+  fatherContactNo: {
+    type: String,
+    required: [true, 'Father contact no. is required'],
+  },
+  motherName: {
+    type: String,
+    required: [true, 'Mother name is required'],
+  },
+  motherOccupation: {
+    type: String,
+    required: [true, 'Mother occupation is required'],
+  },
+  motherContactNo: {
+    type: String,
+    required: [true, 'Mother contact no. is required'],
+  },
+});
+
+const localGuardianSchema = new Schema<localGuardian>({
+  name: {
+    type: String,
+    required: [true, 'Local guardian name is required'],
+  },
+  occupation: {
+    type: String,
+    required: [true, 'Local guardian occupation is required'],
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: [true, 'Local guardian address is required'],
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String, required: true, unique: true },
+  name: {
+    type: userNameSchema,
+    required: [true, 'Student name is required'],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female', 'other'],
+      message: '{VALUE} is not valid',
+    },
+    required: [true, 'Gender is required'],
+  },
+  dateOfBirth: { type: String, required: [true, 'Date of birth is required'] },
+  email: { type: String, required: [true, 'Email is required'], unique: true },
+  contactNo: {
+    type: String,
+    required: [true, 'Student contact no. is required'],
+  },
+  emergencyContactNo: {
+    type: String,
+    required: [true, 'Emergency contact no. is required'],
+  },
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: {
+    type: guardianSchema,
+    required: true,
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: true,
+  },
+  profileImg: {
+    type: String,
+  },
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
+});
+
+// 3. Create a Model.
+export const StudentModel = model<Student>('Student', studentSchema); // 'student' will ben 'students' in MongoDB

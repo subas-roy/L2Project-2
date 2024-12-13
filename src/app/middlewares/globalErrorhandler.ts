fc7904a41b214f71bdf40bf4795fe365e9481891
subas-roy <subasroy46@gmail.com>
2024-12-13 23:11:20 +0600

    Handle Error, AppError, UnhandledRejection, UncaughtException

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -8,12 +8,12 @@ import hangleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
 import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
-  let statusCode = err.statusCode || 500;
-  let message = err.message || 'Internal Server Error';
-
+  let statusCode = 500;
+  let message = 'Something went wrong';
   let errorSources: TErrorSources = [
     {
       path: '',
@@ -41,6 +41,23 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        meessage: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        meessage: err?.message,
+      },
+    ];
   }
 
   // ultimate return
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
 
+let server: Server;
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
@@ -14,3 +17,20 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log('unhandledRejection is detected');
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log('uncaughtException is detected');
+  process.exit(1);
+});
+
+// console.log(x);

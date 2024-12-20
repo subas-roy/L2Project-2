4dab6873a183c806c68ece94c46c64048aff764d
subas-roy <subasroy46@gmail.com>
2024-12-20 14:29:53 +0600

    Create Interface, model, validation for Semester Registration

--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,8 +1,44 @@
-import mongoose from "mongoose";
-import { TSemesterRegistration } from "./semesterRegistration.interface";
+import mongoose, { Schema } from 'mongoose';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
 
-export const semesterRegistrationSchema = new mongoose.Schema.<TSemesterRegistration>(
-  {}
-)
+export const semesterRegistrationSchema =
+  new mongoose.Schema<TSemesterRegistration>(
+    {
+      academicSemester: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+        ref: 'AcademicSemester',
+      },
+      status: {
+        type: String,
+        enum: SemesterRegistrationStatus,
+        default: 'UPCOMING',
+      },
+      startDate: {
+        type: Date,
+        required: true,
+      },
+      endDate: {
+        type: Date,
+        required: true,
+      },
+      minCredit: {
+        type: Number,
+        default: 3,
+      },
+      maxCredit: {
+        type: Number,
+        default: 15,
+      },
+    },
+    {
+      timestamps: true,
+    },
+  );
 
-export const semesterRegistration = mongoose.model<TSemesterRegistration>('SemesterRegistration', semesterRegistrationSchema);
+export const semesterRegistration = mongoose.model<TSemesterRegistration>(
+  'SemesterRegistration',
+  semesterRegistrationSchema,
+);

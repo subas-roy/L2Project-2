9ae4d651d67ead03418a39bfe6f741b4b2dde26e
subas-roy <subasroy46@gmail.com>
2024-12-25 07:44:39 +0600

    Create auth middleware

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -3,17 +3,11 @@ import { AnyZodObject } from 'zod';
 
 const validateRequest = (schema: AnyZodObject) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // validation check
-      //if everything allright next() ->
-      await schema.parseAsync({
-        body: req.body,
-      });
+    await schema.parseAsync({
+      body: req.body,
+    });
 
-      next();
-    } catch (err) {
-      next(err);
-    }
+    next();
   };
 };
 
